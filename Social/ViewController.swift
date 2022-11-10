e2bc08f12cb8dae385e0b1fe358ae43545c96f4b
Ángel González <jan.zelaznog@gmail.com>
2022-11-10 16:59:25 -0600

    Initial Commit

---
+++ Social/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social
+//
+//  Created by Ángel González on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
